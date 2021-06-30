23b7fd93f0df83c1e44e49b041e10abb90a0b257
Minh124 <86700098+Minh124@users.noreply.github.com>
2021-06-30 11:57:06 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    introTitle: 'Hello !',
+    introDesc: `Thích thì thích không thích phải thích,
+    btnIntro: 'hihi',
+    title: 'cậu thích mình phải không? ',
+    desc: 'haha',
+    btnYes: 'Tớ thích cậu:33',
+    btnNo: ' Không, cậu ạ:v',
+    question: 'Tại sao vậy?,
+    btnReply: ' Gửi cho tớ ',
+    Reply: ' tại cậu đáng iu :3',
+    
+}   
+    
+   
+    
+   
+   
+
+
